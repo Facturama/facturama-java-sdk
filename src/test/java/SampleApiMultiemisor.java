f848eead64504cac4007e9c601dd2cb2545c7506
Rafael Dorantes <jesus.dorantes.89@gmail.com>
2022-07-20 15:28:51 -0500

    Actualización 0.3.3
    Se añade NumerosPedimento en conceptos

--- src/test/java/SampleApiMultiemisor.java
+++ src/test/java/SampleApiMultiemisor.java
@@ -112,33 +112,33 @@ public class SampleApiMultiemisor {
         
         
         // Listado de todos los Certificados que tiene cargados el usuario       
-        System.out.println( "Listado de todos los Certificados que tiene cargados el usuario" );
-        List<Csd> lstCsd = facturama.Csd().List();         
-       for(int i=0;i <lstCsd.size();i++)
-       {
-            Csd csdlist = lstCsd.get(i);
-            System.out.println();
-            System.out.println(csdlist.getCertificate());
-            System.out.println(csdlist.getPrivateKey());
-            System.out.println(csdlist.getPrivateKeyPassword());
-            System.out.println(csdlist.getRfc());                   
-       }
- 
-       // Mostrado del certificado especìfico para el RFC AAA010101AAA
-        System.out.println( "Mostrado del certificado especìfico para el RFC EKU9003173C9" );
-        System.out.println();
-        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getCertificate());
-        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getPrivateKey());
-        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getPrivateKeyPassword());
-        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getRfc());
-        
-        System.out.println();
-        System.out.println("ejemplo de CSD terminado");
+//        System.out.println( "Listado de todos los Certificados que tiene cargados el usuario" );
+//        List<Csd> lstCsd = facturama.Csd().List();         
+//       for(int i=0;i <lstCsd.size();i++)
+//       {
+//            Csd csdlist = lstCsd.get(i);
+//            System.out.println();
+//            System.out.println(csdlist.getCertificate());
+//            System.out.println(csdlist.getPrivateKey());
+//            System.out.println(csdlist.getPrivateKeyPassword());
+//            System.out.println(csdlist.getRfc());                   
+//       }
+// 
+//       // Mostrado del certificado especìfico para el RFC AAA010101AAA
+//        System.out.println( "Mostrado del certificado especìfico para el RFC EKU9003173C9" );
+//        System.out.println();
+//        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getCertificate());
+//        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getPrivateKey());
+//        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getPrivateKeyPassword());
+//        System.out.println(facturama.Csd().Retrieve("EKU9003173C9").getRfc());
+//        
+//        System.out.println();
+//        System.out.println("ejemplo de CSD terminado");
     
     }
     
     private static Csd sampleCsdCreate( FacturamaApiMultiemisor facturama) throws IOException, FacturamaException, Exception
-     {  
+    {  
         Csd newCsd = new Csd();
         newCsd.setCertificate("MIIFuzCCA6OgAwIBAgIUMzAwMDEwMDAwMDA0MDAwMDI0MzQwDQYJKoZIhvcNAQELBQAwggErMQ8wDQYDVQQDDAZBQyBVQVQxLjAsBgNVBAoMJVNFUlZJQ0lPIERFIEFETUlOSVNUUkFDSU9OIFRSSUJVVEFSSUExGjAYBgNVBAsMEVNBVC1JRVMgQXV0aG9yaXR5MSgwJgYJKoZIhvcNAQkBFhlvc2Nhci5tYXJ0aW5lekBzYXQuZ29iLm14MR0wGwYDVQQJDBQzcmEgY2VycmFkYSBkZSBjYWRpejEOMAwGA1UEEQwFMDYzNzAxCzAJBgNVBAYTAk1YMRkwFwYDVQQIDBBDSVVEQUQgREUgTUVYSUNPMREwDwYDVQQHDAhDT1lPQUNBTjERMA8GA1UELRMIMi41LjQuNDUxJTAjBgkqhkiG9w0BCQITFnJlc3BvbnNhYmxlOiBBQ0RNQS1TQVQwHhcNMTkwNjE3MTk0NDE0WhcNMjMwNjE3MTk0NDE0WjCB4jEnMCUGA1UEAxMeRVNDVUVMQSBLRU1QRVIgVVJHQVRFIFNBIERFIENWMScwJQYDVQQpEx5FU0NVRUxBIEtFTVBFUiBVUkdBVEUgU0EgREUgQ1YxJzAlBgNVBAoTHkVTQ1VFTEEgS0VNUEVSIFVSR0FURSBTQSBERSBDVjElMCMGA1UELRMcRUtVOTAwMzE3M0M5IC8gWElRQjg5MTExNlFFNDEeMBwGA1UEBRMVIC8gWElRQjg5MTExNk1HUk1aUjA1MR4wHAYDVQQLExVFc2N1ZWxhIEtlbXBlciBVcmdhdGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCN0peKpgfOL75iYRv1fqq+oVYsLPVUR/GibYmGKc9InHFy5lYF6OTYjnIIvmkOdRobbGlCUxORX/tLsl8Ya9gm6Yo7hHnODRBIDup3GISFzB/96R9K/MzYQOcscMIoBDARaycnLvy7FlMvO7/rlVnsSARxZRO8Kz8Zkksj2zpeYpjZIya/369+oGqQk1cTRkHo59JvJ4Tfbk/3iIyf4H/Ini9nBe9cYWo0MnKob7DDt/vsdi5tA8mMtA953LapNyCZIDCRQQlUGNgDqY9/8F5mUvVgkcczsIgGdvf9vMQPSf3jjCiKj7j6ucxl1+FwJWmbvgNmiaUR/0q4m2rm78lFAgMBAAGjHTAbMAwGA1UdEwEB/wQCMAAwCwYDVR0PBAQDAgbAMA0GCSqGSIb3DQEBCwUAA4ICAQBcpj1TjT4jiinIujIdAlFzE6kRwYJCnDG08zSp4kSnShjxADGEXH2chehKMV0FY7c4njA5eDGdA/G2OCTPvF5rpeCZP5Dw504RZkYDl2suRz+wa1sNBVpbnBJEK0fQcN3IftBwsgNFdFhUtCyw3lus1SSJbPxjLHS6FcZZ51YSeIfcNXOAuTqdimusaXq15GrSrCOkM6n2jfj2sMJYM2HXaXJ6rGTEgYmhYdwxWtil6RfZB+fGQ/H9I9WLnl4KTZUS6C9+NLHh4FPDhSk19fpS2S/56aqgFoGAkXAYt9Fy5ECaPcULIfJ1DEbsXKyRdCv3JY89+0MNkOdaDnsemS2o5Gl08zI4iYtt3L40gAZ60NPh31kVLnYNsmvfNxYyKp+AeJtDHyW9w7ftM0Hoi+BuRmcAQSKFV3pk8j51la+jrRBrAUv8blbRcQ5BiZUwJzHFEKIwTsRGoRyEx96sNnB03n6GTwjIGz92SmLdNl95r9rkvp+2m4S6q1lPuXaFg7DGBrXWC8iyqeWE2iobdwIIuXPTMVqQb12m1dAkJVRO5NdHnP/MpqOvOgLqoZBNHGyBg4Gqm4sCJHCxA1c8Elfa2RQTCk0tAzllL4vOnI1GHkGJn65xokGsaU4B4D36xh7eWrfj4/pgWHmtoDAYa8wzSwo2GVCZOs+mtEgOQB91/g==");
         newCsd.setPrivateKey("MIIFDjBABgkqhkiG9w0BBQ0wMzAbBgkqhkiG9w0BBQwwDgQIAgEAAoIBAQACAggAMBQGCCqGSIb3DQMHBAgwggS8AgEAMASCBMh4EHl7aNSCaMDA1VlRoXCZ5UUmqErAbucRFLOMmsAaFNkyWR0dXIAh0CMjE6NpQIMZhQ0HH/4tHgmwh4kCawGjIwERoG6/IH3mCt7u19J5+m6gUEGOJdEMXj976E5lKCd/EG6t6lCq66GE3rgux/nFmeQZvsjLlzPyhe2j+X81LrGudITTjDdgLI0EdbdV9CUJwWbibzrVxjuAVShRh07XPL/DiEw3Wk2+kdy4cfWmMvh0U55p0RKZopNkWuVVSvr3ai7ZNCwHZWDVqkUDpwDDGdyt0kYQ7qoKanIxv/A9wv6ekq0LQ/yLlOcelkxQeb8Glu4RXe+krRvrASw1eBAQ3mvNKpngwF8vtlyoil41PjHUOKALMJtNpywckRRYOk4703ylWIzTfdBlrZ6VmDBjdC5723G1HAx3R/x+o+08++RNiFaN06Ly5QbZZvjnealDfSKz1VKRHWeXggaW87rl4n0SOOWnvabKs4ZWRXTS0dhWK+KD/yYYQypTslDSXQrmyMkpc1Zcb4p9RTjodXxGCWdsR5i5+Ro/RiJvxWwwaO3YW6eaSavV0ROqANQ+A+GizMlxsVjl6G5Ooh6ORdA7jTNWmK44Icgyz6QFNh+J3NibxVK2GZxsQRi+N3HXeKYtq5SDXARA0BsaJQzYfDotA9LFgmFKg9jVhtcc1V3rtpaJ5sab8tdBTPPyN/XT8fA0GxlIX+hjLd3E9wB7qzNR6PZ84UKDxhCGWrLuIoSzuCbr+TD9UCJprsfTu8kr8Pur4rrxm7Zu1MsJRR9U5Ut+O9FZfw4SqGykyTGGh0v1gDG8esKpTW5MKNk9dRwDNHEmIF6tE6NeXDlzovf8VW6z9JA6AVUkgiFjDvLUY5MgyTqPB9RJNMSAZBzrkZgXyHlmFz2rvPqQGFbAtukjeRNS+nkVayLqfQnqpgthBvsgDUgFn03z0U2Svb094Q5XHMeQ4KM/nMWTEUC+8cybYhwVklJU7FBl9nzs66wkMZpViIrVWwSB2k9R1r/ZQcmeL+LR+WwgCtRs4It1rNVkxXwYHjsFM2Ce46TWhbVMF/h7Ap4lOTS15EHC8RvIBBcR2w1iJ+3pXiMeihArTELVnQsS31X3kxbBp3dGvLvW7PxDlwwdUQOXnMoimUCI/h0uPdSRULPAQHgSp9+TwqI0Uswb7cEiXnN8PySN5Tk109CYJjKqCxtuXu+oOeQV2I/0knQLd2zol+yIzNLj5a/HvyN+kOhIGi6TrFThuiVbbtnTtRM1CzKtFGuw5lYrwskkkvenoSLNY0N85QCU8ugjc3Bw4JZ9jNrDUaJ1Vb5/+1GQx/q/Dbxnl+FK6wMLjXy5JdFDeQyjBEBqndQxrs9cM5xBnl6AYs2Xymydafm2qK0cEDzwOPMpVcKU8sXS/AHvtgsn+rjMzW0wrQblWE0Ht/74GgfCj4diCDtzxQ0ggi6yJD+yhLZtVVqmKS3Gwnj9RxPLNfpgzPP01eYyBBi/W0RWTzcTb8iMxWX52MTU0oX9//4I7CAPXn0ZhpWAAIvUmkfjwfEModH7iwwaNtZFlT2rlzeshbP++UCEtqbwvveDRhmr5sMYkl+duEOca5156fcRy4tQ8Y3moNcKFKzHGMenShEIHz+W5KE=");
@@ -146,7 +146,7 @@ public class SampleApiMultiemisor {
         newCsd.setRfc("EKU9003173C9");
         return facturama.Csd().Create(newCsd);
     
-    }   
+    }  
      
     //Test CFDI 3.3
     private static void sampleCfdi( FacturamaApiMultiemisor facturama) throws IOException, FacturamaException, Exception
@@ -233,8 +233,8 @@ public class SampleApiMultiemisor {
         // Se obtiene la factura recien creada
         com.Facturama.sdk_java.Models.Response.Cfdi cfdiCreated = facturama.Cfdis().Create3(cfdi);
 
-        System.out.println( "Se creó exitosamente el cfdi con el folio fiscal: " +  cfdiCreated.getComplement().getTaxStamp().getUuid() ); // CFDI 3.3
-        //System.out.println( "Se creó exitosamente el cfdi con el folio fiscal: " +  cfdiCreated.getComplement().getTaxStamp().getUuid() ); // cfdi 4.0 test
+        System.out.println( "Se creó exitosamente el cfdi con el folio fiscal: " +  cfdiCreated.getComplement().getTaxStamp().getUuid() ); 
+
         
         // Descarga de los archivos de la factura
         //String filePath = "factura"+cfdiCreated.getComplement().getTaxStamp().getUuid();
@@ -321,7 +321,6 @@ public class SampleApiMultiemisor {
             cfdi.setExpeditionPlace("78140");
             
             Receiver  receiver = new Receiver();
-            receiver.setCfdiUse(facturama.Catalogs().CfdiUses("AAA010101AAA").get(0).getValue());
             receiver.setCfdiUse("G03");
             receiver.setName("UNIVERSIDAD ROBOTICA ESPAÑOLA");
             receiver.setRfc("URE180429TM6");
@@ -383,7 +382,7 @@ public class SampleApiMultiemisor {
             
             Receiver  receiver = new Receiver();
             receiver.setRfc("XAXX010101000");
-            receiver.setName("PUBLICO GENERAL");
+            receiver.setName("PUBLICO EN GENERAL");
             receiver.setCfdiUse("S01");
             receiver.setTaxZipCode("30230");
             receiver.setFiscalRegime("616");
@@ -437,7 +436,15 @@ public class SampleApiMultiemisor {
             item.setThirdPartyAccount(thirdPartyAccount);
             item.setCuentaPredial("12345");
             */
-       
+            
+            //Número Pedimento
+            /*
+            ArrayList<String> numerosPedimento=new ArrayList<>();
+            numerosPedimento.add("21  47  3807  8003832");
+            numerosPedimento.add("21  47  3807  8003832");
+            numerosPedimento.add("21  47  3807  8003832");
+            item.setNumerosPedimento(numerosPedimento);
+            */
             
             
             
